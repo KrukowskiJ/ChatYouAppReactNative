cc50569aa8a30bda796af6053ae7d33127419acf
Patryk_CodeHussar <patrykostrowski1411@gmail.com>
2020-11-15 13:16:29 +0100

    chatbox, connect mail, filters

---
+++ app/components/Chatbot.jsx
@@ -0,0 +1,10 @@
+import React, {Component, PureComponent} from 'react';
+import {Image, StyleSheet, Text, View } from 'react-native';
+
+export default function Chatbot(){
+    return(
+        <View>
+
+        </View>
+    );
+}
